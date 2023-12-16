0d9fe5d12b56ad99029eba7c4cf8521c56134567
IndiartoAji13 <indiartoaji13@gmail.com>
2023-12-16 09:58:03 +0700

    first commit

---
+++ src/MathBasic.js
@@ -0,0 +1,61 @@
+// const MathBasic = {
+//    add: (...args) => {
+//       if (args.length !== 2) {
+//          throw new Error('fungsi add hanya menerima dua parameter');
+//       }
+
+//       const [a, b] = args;
+
+//       if (typeof a !== 'number' || typeof b !== 'number') {
+//          throw new Error('fungsi hanya menerima parameter number');
+//       }
+
+//       return a + b
+//    },
+//    subtract: () => {
+
+//    },
+//    multiply: () => {
+
+//    },
+//    divide: () => {
+
+//    }
+// }
+
+
+// module.exports = MathBasic
+
+
+const MathBasic = {
+   _validateArgs(args) {
+      if (args.length !== 2) {
+         throw new Error('fungsi hanya menerima dua parameter');
+      }
+
+      const [a, b] = args;
+
+      if (typeof a !== 'number' || typeof b !== 'number') {
+         throw new Error('fungsi hanya menerima parameter number');
+      }
+      return args;
+   },
+   add(...args) {
+      const [a, b] = this._validateArgs(args);
+      return a + b;
+   },
+   subtract(...args) {
+      const [a, b] = this._validateArgs(args);
+      return a - b;
+   },
+   multiply(...args) {
+      const [a, b] = this._validateArgs(args);
+      return a * b;
+   },
+   divide(...args) {
+      const [a, b] = this._validateArgs(args);
+      return a / b;
+   },
+};
+
+module.exports = MathBasic;
